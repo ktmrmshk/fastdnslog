137072a4ce91b0c2f9f9a49babe42881f88bc9d6
Masahiko Kitamura <ktmrmshk@gmail.com>
2020-02-17 18:21:27 +0900

    mod

--- fastdnslog.py
+++ fastdnslog.py
@@ -24,11 +24,23 @@ class fastdnslog(object):
         return datetime.strptime(str_fmt, '%d/%m/%Y %H:%M:%S')
 
 
+    def init_engine(self, dbhost='localhost', dbport=3306, dbuser='root', dbpasswd='passwd', dbname='fastdnslog'):
+        url = 'mysql+mysqlconnector://{}:{}@{}:{}'.format(dbuser, dbpasswd, dbhost, dbport)
+        self.eng = create_engine(url)
+        self.eng.execute("CREATE DATABASE  IF NOT EXISTS {}".format(dbname))
+        self.eng.execute("USE {}".format(dbname))
+
+    def push(self, tablename='default_table', if_exists='append'):
+        self.df.to_sql(tablename, con=self.eng, if_exists=if_exists, chunksize=1000)
+
 def do_test():
     f=fastdnslog()
-    #f.read('samplelog.gz')
-    f.read('head.txt')
+    f.read('samplelog.gz')
+    #f.read('head.txt')
     print(f.df.head())
+    
+    f.init_engine()
+    f.push()
 
 if __name__ == '__main__':
     do_test()
