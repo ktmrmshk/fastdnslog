6f4cfb9d9948ce9e4e6e57f6dc1202a3f2418073
Masahiko Kitamura <ktmrmshk@gmail.com>
2020-02-18 10:31:45 +0900

    added zone file loader

--- fastdnslog.py
+++ fastdnslog.py
@@ -4,9 +4,30 @@ from datetime import datetime
 import mysql.connector
 from sqlalchemy import create_engine
 
-class fastdnslog(object):
+
+class logkit(object):
     def __init__(self):
+        self.df=None
+
+    def read(self, filenmae):
+        'should be implemented at child class'
         pass
+
+    def init_engine(self, dbhost='localhost', dbport=3306, dbuser='root', dbpasswd='passwd', dbname='fastdnslog'):
+        url = 'mysql+mysqlconnector://{}:{}@{}:{}'.format(dbuser, dbpasswd, dbhost, dbport)
+        self.eng = create_engine(url)
+        self.eng.execute("CREATE DATABASE  IF NOT EXISTS {}".format(dbname))
+        self.eng.execute("USE {}".format(dbname))
+
+    def push(self, tablename, if_exists='append'):
+        self.df.to_sql(tablename, con=self.eng, if_exists=if_exists, chunksize=1000)
+
+
+
+
+class fastdnslog(logkit):
+    def __init__(self):
+        super().__init__()
 
     def read(self, filename):
         names=['subinfo', 'srcip', 'srcport', 'record', 'class', 'type', 'e0flag', 'e0size','dsecflag','tcpflag','res']
@@ -24,23 +45,32 @@ class fastdnslog(object):
         return datetime.strptime(str_fmt, '%d/%m/%Y %H:%M:%S')
 
 
-    def init_engine(self, dbhost='localhost', dbport=3306, dbuser='root', dbpasswd='passwd', dbname='fastdnslog'):
-        url = 'mysql+mysqlconnector://{}:{}@{}:{}'.format(dbuser, dbpasswd, dbhost, dbport)
-        self.eng = create_engine(url)
-        self.eng.execute("CREATE DATABASE  IF NOT EXISTS {}".format(dbname))
-        self.eng.execute("USE {}".format(dbname))
+class zonefiler(logkit):
+    def __init__(self):
+        super().__init__()
 
-    def push(self, tablename='default_table', if_exists='append'):
-        self.df.to_sql(tablename, con=self.eng, if_exists=if_exists, chunksize=1000)
-
-def do_test():
+    def read(self, filename):
+        names = ['record', 'ttl', 'class', 'type', 'val']
+        self.df = pd.read_csv(filename, header=None, comment=';', sep='\t', names=names)
+        self.df['record'] = self.df['record'].apply(lambda x: x[:-1])
+    
+def do_test1():
     f=fastdnslog()
-    f.read('samplelog.gz')
-    #f.read('head.txt')
+    #f.read('samplelog.gz')
+    f.read('head.txt')
     print(f.df.head())
     
     f.init_engine()
-    f.push()
+    f.push('foobar')
+
+def do_test():
+    z=zonefiler()
+    z.read('example.zone')
+    z.init_engine()
+    z.push('zone_sgn_suntory_co_jp')
+
+
+
 
 if __name__ == '__main__':
     do_test()
